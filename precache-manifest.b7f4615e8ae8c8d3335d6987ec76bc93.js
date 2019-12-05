0bf720cf6caa3381835568a34b126c04487901c4
Bikram <bikram@FH-MAC-19-055.local>
2019-12-05 13:15:27 +0530

    Updates

---
+++ precache-manifest.b7f4615e8ae8c8d3335d6987ec76bc93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57160e825ffd2e3d65fac8646f218e0f",
+    "url": "/bikramjethi/bikramjethi.github.io/index.html"
+  },
+  {
+    "revision": "a8928a38a4139869a942",
+    "url": "/bikramjethi/bikramjethi.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aa147bc1d0013cbe0867",
+    "url": "/bikramjethi/bikramjethi.github.io/static/js/2.d14c9a6e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bikramjethi/bikramjethi.github.io/static/js/2.d14c9a6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8928a38a4139869a942",
+    "url": "/bikramjethi/bikramjethi.github.io/static/js/main.c42effc7.chunk.js"
+  },
+  {
+    "revision": "7af6381216a2ca6e180b",
+    "url": "/bikramjethi/bikramjethi.github.io/static/js/runtime-main.ed3558ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bikramjethi/bikramjethi.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
