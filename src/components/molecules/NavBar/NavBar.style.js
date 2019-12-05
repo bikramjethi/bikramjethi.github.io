a53fa8756ef66b30d55c6183a70218d2524b3f4f
Bikram <bikram@FH-MAC-19-055.local>
2019-12-05 16:28:48 +0530

    styling added for basic visibility of navbar

--- src/components/molecules/NavBar/NavBar.style.js
+++ src/components/molecules/NavBar/NavBar.style.js
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 export default styled.nav`
+  ul {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 20px;
+  }
+
+  a {
+    padding: 10px 20px;
+    background: white;
+    border-radius: 5px;
+  }
 `;
