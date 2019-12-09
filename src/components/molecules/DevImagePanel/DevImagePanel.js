5b22da53ddf41427891a711e869c9f956ef52713
Bikram <bikram@FH-MAC-19-055.local>
2019-12-09 12:59:53 +0530

    updated developer image

--- src/components/molecules/DevImagePanel/DevImagePanel.js
+++ src/components/molecules/DevImagePanel/DevImagePanel.js
@@ -1,6 +1,6 @@
 import React from "react";
 import StyledDevImagePanel from "./DevImagePanel.style";
-import introImage from "../../../assets/images/introImage.jpeg";
+import introImage from "../../../assets/images/introImage.png";
 
 const DevImagePanel = ({ className, color }) => {
   return (
