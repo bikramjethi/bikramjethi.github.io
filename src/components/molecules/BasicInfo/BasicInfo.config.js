d878a4d3164023e9e3b5ca3f4d9d5c28f71cf4e7
Bikram <bikram@FH-MAC-19-055.local>
2019-12-10 13:28:16 +0530

    minor content added for resume

--- src/components/molecules/BasicInfo/BasicInfo.config.js
+++ src/components/molecules/BasicInfo/BasicInfo.config.js
@@ -6,8 +6,14 @@ export const bulletConfig = {
   },
   resume: {
     point1: "3.6 years experience",
-    point2: "Developer",
-    point3: "Footballer"
+    point2: "ReactJS",
+    point3: "Redux",
+    point4: "Redux Saga",
+    point5: "HTML",
+    point6: "CSS",
+    point7: "JavaScript",
+    point8: "NextJS",
+    point9: "Webpack"
   },
   projects: {
     point1: "24 years",
--- src/components/templates/Resume/Resume.js
+++ src/components/templates/Resume/Resume.js
@@ -1,10 +1,17 @@
 import React from "react";
 import StyledResume from "./Resume.style";
 import BasicInfo from "../../molecules/BasicInfo/BasicInfo";
+import Paragraph from "../../atoms/Paragraph";
 
 const Resume = () => (
   <StyledResume className="right-div" pageType="resume">
-    <BasicInfo heading="Resume" pageType="resume"></BasicInfo>
+    <BasicInfo heading="Resume" pageType="resume">
+      <Paragraph>
+        Worked as an Experience technology Associate at Publicis.Sapient for 3
+        years. Currently working as FrontEnd developer at fabHotels and leading
+        the TA-SBT Track.
+      </Paragraph>
+    </BasicInfo>
   </StyledResume>
 );
 
