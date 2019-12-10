efc1033e912b78b847d687e0ae8219603831aa08
Bikram <bikram@FH-MAC-19-055.local>
2019-12-10 12:08:38 +0530

    Social media links divided into atoms

---
+++ src/components/atoms/SocialMediaLink/SocialMediaLink.js
@@ -0,0 +1,8 @@
+import React from "react";
+import StyledSocialMediaLink from "./SocialMediaLink.style";
+
+const SocialMediaLink = ({ type }) => (
+  <StyledSocialMediaLink>{type}</StyledSocialMediaLink>
+);
+
+export default SocialMediaLink
--- src/components/molecules/SocialMediaSection/SocialMediaSection.js
+++ src/components/molecules/SocialMediaSection/SocialMediaSection.js
@@ -1,8 +1,14 @@
 import React from "react";
 import StyledSocialMediaSection from "./SocialMediaSection.style";
+import SocialMediaLink from "../../atoms/SocialMediaLink";
 
 const SocialMediaSection = () => (
-  <StyledSocialMediaSection>This is social media</StyledSocialMediaSection>
+  <StyledSocialMediaSection>
+    <SocialMediaLink type="facebook" link="" />
+    <SocialMediaLink type="linkedIn" link="" />
+    <SocialMediaLink type="twitter" link="" />
+    <SocialMediaLink type="insta" link="" />
+  </StyledSocialMediaSection>
 );
 
 export default SocialMediaSection;
