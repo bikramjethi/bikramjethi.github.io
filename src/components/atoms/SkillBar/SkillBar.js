e37475ad9eea335e367781482c548a5887e1f722
Bikram <bikram@FH-MAC-19-055.local>
2019-12-11 00:01:49 +0530

    skillbar config commit

--- src/components/atoms/SkillBar/SkillBar.js
+++ src/components/atoms/SkillBar/SkillBar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import StyledSkillBar from "./SkillBar.style";
 
-const SkillBar = ({ className, color, value, skill }) => (
-  <StyledSkillBar className={className} color={color} value={value}>
-    <span className="skill">{skill}</span>
-    <span className="skill-value">{value}%</span>
-  </StyledSkillBar>
-);
+const SkillBar = ({ className, color, value, skill }) => {
+  return (
+    <StyledSkillBar className={className} color={color} value={value}>
+      <span className="skill">{skill}</span>
+      <span className="skill-value">{value}%</span>
+    </StyledSkillBar>
+  );
+};
 
 export default SkillBar;
--- src/components/templates/Resume/Resume.config.js
+++ src/components/templates/Resume/Resume.config.js
@@ -1,6 +1,6 @@
 export const developerSkills = [
   { value: 95, skill: "HTML" },
   { value: 90, skill: "CSS" },
-  { value: 90, skill: "React JS" },
+  { value: 93, skill: "React JS" },
   { value: 80, skill: "Vanilla JS" },
 ];
