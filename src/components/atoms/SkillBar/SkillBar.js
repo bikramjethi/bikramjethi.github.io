759c30c8efa4129a9d696a9fba9c5bf48869d765
Bikram <bikram@FH-MAC-19-055.local>
2019-12-11 00:30:37 +0530

    skillbar state solution

--- src/components/atoms/SkillBar/SkillBar.js
+++ src/components/atoms/SkillBar/SkillBar.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StyledSkillBar from "./SkillBar.style";
 
 const SkillBar = ({ className, color, value, skill }) => {
+  const [skillValue, setSkillValue] = useState(0);
+  const updateSkillValue = () => {
+    if (skillValue < value) {
+      setTimeout(setSkillValue(skillValue + 1), 200);
+    }
+  };
+  useEffect(() => {
+    // console.log("useEffect called");
+    updateSkillValue();
+    console.log('called useeffect');
+  }, [skillValue, updateSkillValue]);
   return (
-    <StyledSkillBar className={className} color={color} value={value}>
+    <StyledSkillBar className={className} color={color} value={skillValue}>
       <span className="skill">{skill}</span>
-      <span className="skill-value">{value}%</span>
+      <span className="skill-value">{skillValue}%</span>
     </StyledSkillBar>
   );
 };
