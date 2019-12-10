d60a4c73f90f0b39d400be92ecd276ac0f386bed
Bikram <bikram@FH-MAC-19-055.local>
2019-12-11 00:59:11 +0530

    reduced re-renders for components

--- src/components/atoms/SkillBar/SkillBar.js
+++ src/components/atoms/SkillBar/SkillBar.js
@@ -3,16 +3,11 @@ import StyledSkillBar from "./SkillBar.style";
 
 const SkillBar = ({ className, color, value, skill }) => {
   const [skillValue, setSkillValue] = useState(0);
-  const updateSkillValue = () => {
-    if (skillValue < value) {
-      setTimeout(setSkillValue(skillValue + 1), 200);
-    }
-  };
   useEffect(() => {
-    // console.log("useEffect called");
-    updateSkillValue();
-    console.log('called useeffect');
-  }, [skillValue, updateSkillValue]);
+    console.log("useEffect called");
+    if (skillValue + 3 <= value) setSkillValue(skillValue + 3);
+    else setSkillValue(value);
+  }, [skillValue,value]);
   return (
     <StyledSkillBar className={className} color={color} value={skillValue}>
       <span className="skill">{skill}</span>
