e96fc1eff0fb68dce51b604e5a82959288b32714
Bikram <bikram@FH-MAC-19-055.local>
2019-12-11 11:42:26 +0530

    skillbar view break on mobile corrected

--- src/components/molecules/Skills/Skills.style.js
+++ src/components/molecules/Skills/Skills.style.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export default styled.div`
+  padding-right: 10px;
   .skill-heading {
     margin-bottom: 40px;
     color: #fff;
-    font-size: 2.5rem;
+    font-size: 2rem;
   }
   .skill-bar {
     margin-bottom: 40px;
